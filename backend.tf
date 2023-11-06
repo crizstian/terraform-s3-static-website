e9701f01decaba526683ed24c78e549b595b004f
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-11-06 10:42:14 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state2465"
-    key    = "Terraform-State"
-    region = "ap-south-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state2465"
+#    key    = "Terraform-State"
+#    region = "ap-south-1"
+#  }
+#}
